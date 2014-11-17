0f9fef92650886058f5a2890e2e14712ab55a69d
jesca <jesca116@gmail.com>
2014-11-17 06:28:31 -0800

    debugging dns

--- firewall.py
+++ firewall.py
@@ -3,7 +3,6 @@
 from main import PKT_DIR_INCOMING, PKT_DIR_OUTGOING
 
 import struct
-import socket
 
 
 # TODO: Feel free to import any Python standard moduless as necessary.
@@ -76,8 +75,7 @@ class Packet:
         return struct.unpack("!B", pkt[byte_begin:(byte_begin + 1)])[0]
 
 class Rule:
-    def __init__(self, string, geoip_list):
-        self.geoip_list = geoip_list
+    def __init__(self, string):
         rule_line = string.split(" ")
 
         #cleanup extra spaces
@@ -105,6 +103,7 @@ class Rule:
 
 
     def compare(self,cur_packet):
+
         print 'packet_proto:', cur_packet.pkt_proto, ' packet_ip: ', cur_packet.ext_ip, ' packet_port: ',  cur_packet.port
         print 'rule_proto:', self.proto, 'rule_ip', self.ext_ip, ' rule_port:', self.ext_port
         #if packet protocol is not tcp, icmp, or udp, just pass
@@ -117,9 +116,24 @@ class Rule:
         if (self.proto == 'dns'):
             #You apply DNS rules only for DNS query packets
             dns_rule = False
+            byte_dns_begin = cur_packet.next_header_begin + 8
+            byte_dns_header_ends = cur_packet.next_header_begin + 8 + 12
             # It is an outgoing UDP packet with destination port 53
+            print "dns pkt_dir: ", cur_packet.pkt_dir, 'port: ', cur_packet.port
+	    qd_count = struct.unpack("!B", cur_packet.pkt[(byte_dns_begin + 4):(byte_dns_begin + 5)])[0]
+            print 'got qd count: ', qd_count
+            print 'packet label thing'
+            qname_index = byte_dns_header_ends
+            print ord(cur_packet.pkt[qname_index])
+            while ord(cur_packet.pkt[qname_index])!= 00:
+
+               print ord(cur_packet.pkt[qname_index])
+
+               qname_index += 1
+
 
             if (cur_packet.pkt_dir == 'outgoing' and cur_packet.port == 53):
+
                 #dns information after ipv4 header and udp header = ~20 bytes + 8 bytes
                 byte_dns_begin = cur_packet.next_header_begin + 8
                 byte_dns_header_ends = cur_packet.next_header_begin + 8 + 12
@@ -138,13 +152,7 @@ class Rule:
             if (dns_rule == False):
                 return 0
             else:
-                print "dns pkt_dir: ", cur_packet.pkt_dir, 'port: ', cur_packet.port
-                byte_dns_begin = cur_packet.next_header_begin + 8
-                byte_dns_header_ends = cur_packet.next_header_begin + 8 + 12
-                qd_count = struct.unpack("!B", cur_packet.pkt[(byte_dns_begin + 4):(byte_dns_begin + 5)])[0]
-                print cur_packet.pkt[byte_dns_header_ends:byte_dns_header_ends + 2]
-
-
+                pass#criteria met, do domain name compare here!!!!
 
 
         else:
@@ -152,10 +160,10 @@ class Rule:
             if (self.proto == cur_packet.pkt_proto):
                 print 'protos same'
                 compare_array.append(1)
-            if self.port_compare(self.ext_port, cur_packet.port):
+            if (self.port_compare(self.ext_port,cur_packet.port)==1):
                 print 'ports same'
                 compare_array.append(1)
-            if self.ext_ip_compare(self.ext_ip, cur_packet.ext_ip):
+            if (self.ext_ip_compare(self.ext_ip, cur_packet.ext_ip) == 1):
                 print 'ext same'
                 compare_array.append(1)
 
@@ -174,9 +182,10 @@ class Rule:
             return 0
 
 
-    def port_compare(self, rule_port, packet_port):
-        print 'comparing ports:', rule_port, packet_port
+    def port_compare(self,rule_port, packet_port):
+        print 'comparing ports:', packet_port, rule_port
         if (rule_port == 'any' or rule_port == 'any\n'):
+            print 'rule port is any'
             return 1
 
         #shouldn't reach here because compare should have returned by now
@@ -203,29 +212,24 @@ class Rule:
 
     def ext_ip_compare(self, rule_ip, packet_ip):
         slash_position = rule_ip.find('/')
-        if (rule_ip == 'any' or rule_ip == 'any\n'):
+        if (rule_ip == 'any'):
             return 1
-        elif (len(rule_ip) == 2):
+        elif (rule_ip.isupper() and len(rule_ip) == 2):
             #country code
-            print ("checking if country matches: ", rule_ip)
-            print ("packet ip: ", socket.inet_ntoa(struct.pack("!I", packet_ip)))
-            print("survey says: ", self.does_ip_match_country2(packet_ip, rule_ip, self.geoip_list))
-            return self.does_ip_match_country2(packet_ip, rule_ip, self.geoip_list)
+            return find_country2(packet_ip) == rule_ip.upper()
         elif (slash_position != -1):
             #range of packets
             print("range mode")
-            set_bits = int(rule_ip[slash_position + 1:], 10)
+            set_bits = rule_ip[slash_position + 1:]
             mask = form_mask(set_bits)
             min_str = rule_ip[:slash_position]
             min_int = ip_to_int(min_str)
 
             max_int = min_int | mask
-
-            packet_ip = ip_to_int(packet_ip)
-            return packet_ip >= min_int and packet_ip <= max_int
+            return packet_ip >= lower_int and packet_ip <= upper_int
         else:
             #rule is just a regular ip
-            int_rule_ip = self.ip_to_int(rule_ip)
+            int_rule_ip = ip_to_int(rule_ip)
             return int_rule_ip == packet_ip
 
 
@@ -244,7 +248,7 @@ class Rule:
 
     #compares two ip values in string format
     #return 1 if 1st ip is greater, -1 if 2nd ip is greater
-    def ip_compare(self, ip1, ip2):
+    def ip_compare(ip1, ip2):
         ip1_split = ip1.split('.')
         ip2_split = ip2.split('.')
         i = 0
@@ -260,40 +264,14 @@ class Rule:
         return 0
 
 
-    def does_ip_match_country(self, ip_int, country, geoip_list):
-        ip_str = socket.inet_ntoa(struct.pack("!I", ip_int))
-        coun_upper = country.upper()
-
+    def find_country2(ip, geoip_list):
         for line in geoip_list:
-            if (line[2] == coun_upper):
-                min_cmp = self.ip_compare(ip_str, line[0])
-                max_cmp = self.ip_compare(ip_str, line[1])
-                if ((min_cmp == 1 and max_cmp == -1) or min_cmp == 0 or max_cmp == 0):
-                    return 1
-        return 0
-
-    def does_ip_match_country2(self, ip_int, country, geoip_list):
-        ip_str = socket.inet_ntoa(struct.pack("!I", ip_int))
-        coun_upper = country.upper()
-        for line in geoip_list:
-            if (line[2][0] == coun_upper[0] and line[2][1] == coun_upper[1]):
-                #print("line[2]: ", line[2])
-                min_cmp = self.ip_compare(ip_str, line[0])
-                max_cmp = self.ip_compare(ip_str, line[1])
-                if (line[0] <= ip_str and line[1] >= ip_str):
-                    return 1
-        return 0
-
-
-    def find_country2(self, ip, geoip_list):
-        ip_str = socket.inet_ntoa(struct.pack("!I", ip))
-        for line in geoip_list:
-            min_cmp = self.ip_compare(ip_str, line[0])
-            max_cmp = self.ip_compare(ip_str, line[1])
+            min_cmp = ip_compare(ip, line[0])
+            max_cmp = ip_compare(ip, line[1])
             if ((min_cmp == 1 and max_cmp == -1) or min_cmp == 0 or max_cmp == 0):
                 return line[2]
 
-    def ip_to_int(self, ip_str):
+    def ip_to_int(ip_str):
         split = ip_str.split('.')
         return (int(split[0]) * 16777216) + (int(split[1]) * 65536) + (int(split[2]) * 256) + (int(split[3]))
 
@@ -303,15 +281,13 @@ class Firewall:
     def __init__(self, config, iface_int, iface_ext):
         self.iface_int = iface_int
         self.iface_ext = iface_ext
-        self.ip_list = self.getGeoList()
-
         self.rule_list = self.makeRuleList(config)
 
         print ("rule_list:")
         for rule in self.rule_list:
             print(rule.verdict, rule.proto, rule.ext_ip, rule.ext_port, rule.domain_name)
 
-
+        self.ip_list = self.getGeoList()
 
     def handle_packet(self, pkt_dir, pkt):
         # TODO: Your main firewall code will be here.
@@ -372,7 +348,7 @@ class Firewall:
         i -= 1
         while (i > -1):
             rule_line = rule_str_list[i]
-            rule_list.append(Rule(rule_line, self.ip_list))
+            rule_list.append(Rule(rule_line))
             i -= 1
 
         return rule_list
